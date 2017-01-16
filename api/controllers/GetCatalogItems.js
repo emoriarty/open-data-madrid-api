53551322b23fdb833d701ad7e8f7932e2fef8c53
Enrique Arias <enric@Enriques-MacBook-Pro.local>
2017-01-16 19:28:24 +0100

    Catalog items Pagintation

--- api/controllers/GetCatalogItems.js
+++ api/controllers/GetCatalogItems.js
@@ -1,9 +1,22 @@
 import values from 'lodash/fp/values';
-import keys from 'lodash/fp/keys';
+import sortBy from 'lodash/fp/sortBy';
+import slice from 'lodash/fp/slice';
+import eq from 'lodash/fp/eq';
+import last from 'lodash/fp/last';
 
 const getCatalogItems = (req, res, next) => {
-  const { data: { items } } = req.app.locals;
-  res.json(values(items));
+  const { data } = req.app.locals;
+  const { params: { page } } = req.swagger;
+  const items = sortBy(['title'], values(data.items));
+  const result = slice(
+    page.value ? page.value * 25 : 0,
+    page.value * 25 + 25,
+    items);
+
+  res.json({
+    more: !eq(last(result), last(items)),
+    items: result
+  });
 };
 
 module.exports = {
